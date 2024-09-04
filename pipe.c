7aa42dfd91b36bde3da2d5f203ce981dd418712a
Benjamin Xie <42423568+benjaminxxie@users.noreply.github.com>
2024-09-04 11:05:53 -0700

    Add files via upload

--- pipe.c
+++ pipe.c
@@ -3,78 +3,83 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <errno.h>
 
-int main(int argc, char *argv[])
-{
-	// TODO: it's all yours
+int main(int argc, char *argv[]) {
+    int fd_track = STDIN_FILENO;
 
-	int prev = STDIN_FILENO;
-  	int fds[2];
+    if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            int pipefd[2];
 
-  	// Check arguments
-  	if (argc <= 1) {
-    	exit(EINVAL);
-  	}
+            if (i < argc - 1) {
+                if (pipe(pipefd) == -1) {
+                    //perror("pipe");
+                    exit(EXIT_FAILURE);
+                }
+            }
 
-  	// Failed pipe creation
-  	if (pipe(fds) != 0) {
-    	exit(EXIT_FAILURE);
-  	}
+            int pid = fork();
 
-  	// Iterate through arguments
-  	for (int i = 1; i < argc; i++) {
-    	pid_t pid = fork();
+            if (pid == 0) {
+                // Child process
+                if (i > 1) {
+                    // If on the second iteration, set input as the last output
+                    if (dup2(fd_track, STDIN_FILENO) == -1) {
+                        //perror("dup2");
+                        exit(EXIT_FAILURE);
+                    }
+                    close(fd_track);  // Close the old file descriptor
+                }
 
-    	// Failed fork
-    	if (pid == -1) {
-      		exit(EXIT_FAILURE);
-    	}
+                if (i < argc - 1) {
+                    // Redirect output to STDOUT and store output for the next iteration
+                    if (dup2(pipefd[1], STDOUT_FILENO) == -1) {
+                        //perror("dup2");
+                        exit(EXIT_FAILURE);
+                    }
+                    fd_track = pipefd[0];
+                    close(pipefd[0]);  // Close unused read end of the pipe
+                }
 
-    	// Succeeded fork in child process
-   		else if (pid == 0) {
-      	// arg reads from standard input
-      		dup2(prev, STDIN_FILENO);
-      		// Set last arg output to standard output
-      		if (i != argc - 1) {
-			dup2(fds[1], STDOUT_FILENO);
-      		}
-      // Execute command
-      	execlp(argv[i], argv[i], NULL);
+                close(pipefd[1]);  // Close write end of the pipe in the child
 
-      	return 1;
-    	}
+                execlp(argv[i], argv[i], NULL);
+                //perror("execlp");
+                exit(EXIT_FAILURE);
+            } else if (pid > 0) {
+                // Parent process
+                int new_pid = pid;
+                int status = 0;
+                if (waitpid(new_pid, &status, 0) == -1) {
+    				exit(22);
+				}
 
-    	// Parent process
-    	else {
-      		int st = 0;
-      		waitpid(pid, &st, 0);
-      		if (!WIFEXITED(st) || WEXITSTATUS(st) != 0) {
-				return WEXITSTATUS(st);
-      	}
+				if (WIFEXITED(status)) {
+    				int exit_status = WEXITSTATUS(status);
+    				if (exit_status != 0) {
+        				exit(22);
+   					}
+				} 
+				else {
+    				exit(EXIT_FAILURE);
+				}
 
-      	if (prev != STDIN_FILENO) {
-			close(prev);
-      	}
+                if (i < argc - 1) {
+                    close(pipefd[1]);  // Close write end of the pipe in the parent
+                }
 
-      	// Reset previous file descriptor
-      		prev = fds[0];
+                // Store the input for the next iteration
+                fd_track = pipefd[0];
+            } else {
+                // Error
+                //perror("fork");
+                exit(EXIT_FAILURE);
+            }
+        }
+    } else {
+        // Not enough arguments
+        exit(22);
+    }
 
-      	close(fds[1]);
-
-      	// Create pipe for next arg if exists
-      	pipe(fds);
-    	}
-  	}
-  	// Close remaining file descriptors
-  	close(fds[0]);
-  	close(fds[1]);
-
-  	// Final cleanup
-  	if (argc >= 2) {
-    	close(prev);
-  	}
-  	else {
-    	return 0;
-  	}
+    return 0;
 }
